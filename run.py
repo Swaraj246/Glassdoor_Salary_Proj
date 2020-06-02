cb1d5bb6129b84a37d904446a406fa3bcb274d5c
unknown <swaraj24698panchal@gmail.com>
2020-06-02 14:21:44 -0500

    uploaded data collection file

---
+++ run.py
@@ -0,0 +1,11 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Jun  1 19:13:15 2020
+
+@author: Swaraj Panchal
+"""
+import glassdoor_scrapper as gs
+import pandas as pd
+path = "C:/Users/13129/Documents/Glassdoor_Salary_Proj/chromedriver"
+
+df = gs.get_jobs('data scientist', 10, False, path, 15)
